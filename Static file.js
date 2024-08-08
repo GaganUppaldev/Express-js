0de63b33c2c4228bc8eb9a42a02f69b335ff0c6a
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-08 23:57:15 +0530

    Update Static file.js
    important points

--- Static file.js
+++ Static file.js
@@ -1,1 +1,3 @@
 // Here i will use previous code But server basic.html which have a form and it is inside public folder so way to serve static files will be here
+//First step is you need to create folder name public than put files inside it ypu want to serve here otherwise you face errors.
+//You need to type url like this (http://localhost:4000/static/basic.html) , need to clarify that basic.html  and i will add a baisc.html here also
