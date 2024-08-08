6c0377225f326a02c8d73daecc414e7384aafa45
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-08 23:59:43 +0530

    Update Static file.js
    re,oved unwanted comments

--- Static file.js
+++ Static file.js
@@ -2,13 +2,10 @@
 //First step is you need to create folder name public than put files inside it ypu want to serve here otherwise you face errors.
 //You need to type url like this (http://localhost:4000/static/basic.html) , need to clarify that basic.html  and i will add a baisc.html here also
 
-//I have knowledge of Django so i can say here that biggest problem beginner will face is of Auto Restart of server after saving file so I sugess you to install Nodemon first and move next .
-//Installing Nodemon is not very difficult just install for user terminal and in my case i am using bash and installed nodemon on that But i had installed it for command propt and used for few days  
 
-//const express = require('express');
 
 import express from 'express'; 
-const app = express();// i forgot to create variable for express
+const app = express();//
 const port = 4000
 
 app.get('/', (req, res) => {  
@@ -16,24 +13,24 @@ app.get('/', (req, res) => {
   
 }) 
 
-app.get('/home',(req,res) => {       //when you add /home to local host url like this http://localhost:4000/contact you will see This is home section means got routed to home page
+app.get('/home',(req,res) => {      
   res.send('This is home section')
 })
 
-app.get('/About',(req,res) => {      // same as above just value changed
+app.get('/About',(req,res) => {      
   res.send('This about section')
 })
 
-app.get('/contact',(req,res) => {    // same as above just value changed
+app.get('/contact',(req,res) => {    
   res.send('This is contact section')
 })
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public')) //This is code for what you are here
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`) //defining port
+  console.log(`Example app listening on port ${port}`) 
 }) 
 
 
