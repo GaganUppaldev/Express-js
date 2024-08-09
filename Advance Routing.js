547a6e50bce9312b07f6eaf506634fa365c7bf93
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-09 23:40:08 +0530

    Update Advance Routing.js
    used very power app.all to perform tasks with multiple type requests

--- Advance Routing.js
+++ Advance Routing.js
@@ -1,1 +1,20 @@
 //This file cover  advance routing concepts form  official documentation
+
+import express from 'express'; 
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('GET request to the homepage')
+})
+
+app.all('/secret', (req, res, next) => {
+  res.send("you can watch secret inside whole webpage")
+  console.log('Accessing the secret section ...')
+  next() // pass control to the next handler
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
